ec5e46f289ad477b3353fe26a03607fe056cb0b1
Linda <linda.kibugi@student.moringaschool.com>
2024-03-03 16:44:22 -0500

    updated

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -4,7 +4,7 @@ function Banner(){
         <div className="banner">
             <div className="banner-text">
                 <h2>Find what fits you best</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, accusamus similique aliquid officiis perferendis eveniet adipisci provident dolorem</p>
+                <p>Welcome to our Fenty-wear store! Explore a world of convenience and variety in just a few clicks. Start shopping now!</p>
                 <button className="banner-btn">Shop now</button>
             </div>
             <div className="banner-image">
